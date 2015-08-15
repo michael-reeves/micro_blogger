629800eaaf10e20806f141512ac12dfa9323aaa8
Michael Reeves <mreeves68@yahoo.com>
2015-08-15 13:22:21 -0600

    Add About page and set up common title template

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | MicroBlogger"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | MicroBlogger"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | MicroBlogger"
+  end
 end
