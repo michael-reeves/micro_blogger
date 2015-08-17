021f646b5b8dfc3dbfe8741e40a64e54c019b28b
Michael Reeves <mreeves68@yahoo.com>
2015-08-17 10:27:04 -0600

    Complete user model with additional test and more complex email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,7 +42,8 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should not accept invalid addresses" do
     valid_addresses = %w[ user@example,com USER_at_foo.COM user.name@example.
-                          foo@baz_baz.com foo@bar+bar.com foo@@example.com ]
+                          foo@baz_baz.com foo@bar+bar.com foo@@example.com
+                          foo@bar..com ]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
@@ -54,6 +55,13 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should be present (nonblank)" do
