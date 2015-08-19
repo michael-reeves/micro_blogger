8a4211e711456b2c3ecc363bf1d70c70d27c020d
Michael Reeves <mreeves68@yahoo.com>
2015-08-19 15:41:53 -0600

    Implement remember token functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
 
@@ -11,6 +13,23 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # Remembers a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password(self.remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 
   # Returns the hash digest of the given string
   def User.digest(string)
@@ -18,4 +37,10 @@ class User < ActiveRecord::Base
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -74,4 +74,8 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return fale for a user with nil digest" do
+    assert_not @user.authenticated?("")
+  end
+
 end
