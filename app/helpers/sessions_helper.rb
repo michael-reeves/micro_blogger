f50cb0c782fba77fb56379ef9df7756b0a7675aa
Michael Reeves <mreeves68@yahoo.com>
2015-08-21 12:25:40 -0600

    Require the correct user to edit/update user profile

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user    
+  end
+
   def current_user
     # @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
